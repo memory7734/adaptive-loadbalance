4bfb90b932db9dd6cdf0157b162ffd481ecdb4f7
温礼 <wenli.zwj@alibaba-inc.com>
2019-06-25 13:22:07 +0800

    '删除掉Atomic类，同时改用数组处理数据，增加对rt与thread两种维度的对比'

--- workspace-gateway/src/main/java/com/aliware/tianchi/CallbackListenerImpl.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/CallbackListenerImpl.java
@@ -2,8 +2,6 @@ package com.aliware.tianchi;
 
 import org.apache.dubbo.rpc.listener.CallbackListener;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 /**
  * @author daofeng.xjf
  * <p>
@@ -18,25 +16,22 @@ public class CallbackListenerImpl implements CallbackListener {
     public void receiveServerMsg(String msg) {
         String[] tokens = msg.split("#");
         if (tokens.length < 4) return;
-        String host = tokens[0];
+        int port = Integer.valueOf(tokens[0]);
         int thread = Integer.valueOf(tokens[1]);
         int active = Integer.valueOf(tokens[2]);
         long rtt = Long.valueOf(tokens[3]);
-
-        if (UserLoadBalance.threadMap.get(host) == null) {
-            UserLoadBalance.threadMap.put(host, thread);
-            UserLoadBalance.activeChanged.set(true);
+        long lastRtt = Long.valueOf(tokens[4]);
+        if (port == 0) return;
+        int index = (port - 20870) / 10;
+        UserLoadBalance.averageRttArray[index] = rtt;
+        UserLoadBalance.lastRttArray[index] = lastRtt;
+        UserLoadBalance.threadArray[index] = thread;
+        for (;;){
+            if(UserLoadBalance.activeChanged.compareAndSet(false, true)){
+                UserLoadBalance.activeArray[index] = active;
+                break;
+            }
         }
-        AtomicInteger remainder = UserLoadBalance.remainderMap.get(host);
-        if (remainder == null) {
-            UserLoadBalance.remainderMap.putIfAbsent(host, new AtomicInteger());
-            remainder = UserLoadBalance.remainderMap.get(host);
-        }
-        if (thread - active != remainder.get()) {
-            remainder.set(thread - active);
-            UserLoadBalance.activeChanged.set(true);
-        }
-        UserLoadBalance.rttMap.put(host, rtt);
     }
 
 }
