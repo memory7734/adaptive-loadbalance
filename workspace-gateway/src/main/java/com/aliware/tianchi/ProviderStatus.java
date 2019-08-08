8288fca22a87005785133a8e4040a8334dc0e1c0
memory7734 <memory7734@gmail.com>
2019-08-09 02:26:21 +0800

    update

--- workspace-gateway/src/main/java/com/aliware/tianchi/ProviderStatus.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/ProviderStatus.java
@@ -8,19 +8,20 @@ public class ProviderStatus {
     static ProviderStatus[] providers = new ProviderStatus[3];
     static ConcurrentSkipListSet<ProviderThread> queue = new ConcurrentSkipListSet<>(Comparator.comparing(o -> o.rtt));
 
-    public ProviderStatus(int port, int maxThreads) {
+    ProviderStatus(int port, int maxThreads) {
         for (int i = 0; i < maxThreads; i++) {
             queue.add(new ProviderThread(port, 50));
         }
     }
 
     public static void response(int port, long rtt) {
-        if (rtt < 10) {
+        if (rtt < 3) {
             queue.pollLast();
             queue.add(new ProviderThread(port, rtt));
             queue.add(new ProviderThread(port, 50));
         } else {
             queue.add(new ProviderThread(port, rtt));
         }
+
     }
 }
